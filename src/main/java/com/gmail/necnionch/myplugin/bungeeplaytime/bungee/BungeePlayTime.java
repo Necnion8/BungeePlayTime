f5babd8846b5f667f331e6802c0646918701838a
BuildTools <unconfigured@null.spigotmc.org>
2021-10-03 14:59:11 +0900

    first commit

---
+++ src/main/java/com/gmail/necnionch/myplugin/bungeeplaytime/bungee/BungeePlayTime.java
@@ -0,0 +1,16 @@
+package com.gmail.necnionch.myplugin.bungeeplaytime.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class BungeePlayTime extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
