db0279b83303a3880af2b736d55706aa4db11c4c
Necnion8 <necnionch@gmail.com>
2022-09-19 18:27:07 +0900

    Fix: 接続完了しない状態でプレイヤーが切断した時に発生する例外の対策 (PlayerDisconnectEvent)

--- src/main/java/com/gmail/necnionch/myplugin/bungeeplaytime/bungee/listeners/PlayerListener.java
+++ src/main/java/com/gmail/necnionch/myplugin/bungeeplaytime/bungee/listeners/PlayerListener.java
@@ -2,6 +2,7 @@ package com.gmail.necnionch.myplugin.bungeeplaytime.bungee.listeners;
 
 import com.gmail.necnionch.myplugin.bungeeplaytime.bungee.BungeePlayTime;
 import com.gmail.necnionch.myplugin.bungeeplaytime.common.AFKState;
+import com.google.common.collect.Sets;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ChatEvent;
@@ -10,9 +11,12 @@ import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
+import java.util.Set;
+
 
 public class PlayerListener implements Listener {
     private final BungeePlayTime plugin;
+    private final Set<ProxiedPlayer> joinedPlayers = Sets.newHashSet();
 
     public PlayerListener(BungeePlayTime plugin) {
         this.plugin = plugin;
@@ -25,6 +29,8 @@ public class PlayerListener implements Listener {
         ServerInfo fromServer = (player.getServer() != null) ? player.getServer().getInfo() : null;
         ServerInfo toServer = event.getServer().getInfo();
 
+        joinedPlayers.add(player);
+
         plugin.insertPlayer(player, System.currentTimeMillis(), toServer.getName(), AFKState.UNKNOWN);
         if (fromServer != null && fromServer.getPlayers().size() <= 1) {
             plugin.getMessenger().removeActive(fromServer);
@@ -35,6 +41,10 @@ public class PlayerListener implements Listener {
     @EventHandler
     public void onQuit(PlayerDisconnectEvent event) {
         ProxiedPlayer player = event.getPlayer();
+
+        if (!joinedPlayers.remove(player))  // 即抜け対応
+            return;
+
         plugin.removePlayer(player.getUniqueId());
 
         if (player.getServer() != null) {
